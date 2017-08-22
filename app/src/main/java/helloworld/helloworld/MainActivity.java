7845a5bbb0fb73fd8180aea1a12c25d7847dd791
Rogiel <rogiel2009@gmail.com>
2017-08-22 18:33:11 -0300

    verificacao de senhas

--- app/src/main/java/helloworld/helloworld/MainActivity.java
+++ app/src/main/java/helloworld/helloworld/MainActivity.java
@@ -3,21 +3,45 @@ package helloworld.helloworld;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button registrarBnt;
-
+    CheckBox mostarSenha;
+    TextView senhaET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       senhaET =  (TextView) findViewById(R.id.idSenhaUser);
 
         registrarBnt = (Button) findViewById(R.id.button);
+        mostarSenha = (CheckBox) findViewById(R.id.mostraSenha);
+
+        mostarSenha.setOnClickListener(new View.OnClickListener() {
+        @Override
+         public void onClick(View v) {
+                if (mostarSenha.isChecked()){
+                    senhaET.setTransformationMethod(null);
+                }
+                else
+                {
+                    senhaET.setTransformationMethod(new PasswordTransformationMethod());
+                }
+
+            }
+        }
+
+        );
+
         registrarBnt.setOnClickListener(new View.OnClickListener() {
             @Override
              public void onClick(View v) {
--- app/src/main/java/helloworld/helloworld/SegundaTela.java
+++ app/src/main/java/helloworld/helloworld/SegundaTela.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by computeiro on 22/08/17.
@@ -11,8 +14,38 @@ import android.widget.Button;
 
 public class SegundaTela extends AppCompatActivity {
 
+
+    Button proximoVerif;
+    EditText senhaET;
+    EditText senhaRepetida;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_tela);
+
+        proximoVerif = (Button) findViewById(R.id.idproximo);
+        senhaET =  (EditText) findViewById(R.id.idsenha);
+        senhaRepetida = (EditText) findViewById(R.id.idrepetSenha);
+
+
+
+        proximoVerif.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                      String  senha  =  senhaET.getText().toString();
+                      String  repetsenha = senhaRepetida.getText().toString();
+                      if (!senha.equals(repetsenha))
+                      {
+                          Toast.makeText(SegundaTela.this, getString(R.string.erro_senha), Toast.LENGTH_SHORT).show();
+                      }
+
+                    }
+                }
+        );
+
+
+
     }
 }
